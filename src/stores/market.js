6dfe662c6b00b5fe8716eac387608816ed25d4a1
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2024-07-26 20:11:13 +0700

    add new data rules and timestamp

--- src/stores/market.js
+++ src/stores/market.js
@@ -1,13 +1,15 @@
 import { reactive } from 'vue'
 import { defineStore } from 'pinia'
 import { db } from '@/firebase'
-import { onSnapshot, doc, updateDoc } from "firebase/firestore"
+import { onSnapshot, doc, updateDoc, Timestamp } from "firebase/firestore"
 
 export const useMarketStore = defineStore('market', () => {
 
   const market = reactive({
     status: '',
-    information: ''
+    information: '',
+    rules: '',
+    timestamp: ''
   })
 
   const marketCollectionRef = doc(db, 'market', 'data')
@@ -16,10 +18,13 @@ export const useMarketStore = defineStore('market', () => {
     onSnapshot(marketCollectionRef, (doc) => {
       market.status = doc.data().status
       market.information = doc.data().information
+      market.rules = doc.data().rules
+      market.timestamp = doc.data().timestamp
     })
   }
 
   function editMarket () {
+    market.timestamp = Timestamp.now()
     updateDoc(marketCollectionRef, market)
   }
 
